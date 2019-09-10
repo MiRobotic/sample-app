fb6c13a427ac4ed67f2486f803e8eadeebdacb06
priyanka mali <priyanka@mirobotic.sg>
2019-09-10 10:46:07 +0800

    update

---
+++ sample/src/main/java/com/robotemi/sdk/sample/MyBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.robotemi.sdk.sample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.out.println();
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
